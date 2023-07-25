319c7f98fcedce1a7c3350140292d469be0d9a97
octo127 <26832968+octo127@users.noreply.github.com>
2023-07-25 09:33:24 +0900

    Update InputTest

--- Assets/InputTest.cs
+++ Assets/InputTest.cs
@@ -5,6 +5,7 @@ public class InputTest : MonoBehaviour
 {
     private void Update()
     {
-        Debug.Log($"Left Command Key: {Keyboard.current.leftCommandKey.isPressed}");
+        Debug.Log($"Left Command Key (New): {Keyboard.current.leftCommandKey.isPressed}");
+        Debug.Log($"Left Command Key (Old): {Input.GetKey(KeyCode.LeftCommand)}");
     }
 }
